924b4ff0ac719c681812a68228de0e27d5ef3029
Contoso <91586643+Acontoso@users.noreply.github.com>
2025-06-05 15:54:26 +1000

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,13 @@
 data "aws_kms_key" "ssm_kms_alias" {
   key_id = "alias/cmk-ssm"
 }
+
+data "aws_s3_bucket" "artefact_bucket" {
+  bucket = "security-terraform-state-weshealth"
+}
+
+data "aws_s3_object" "lambda_zip" {
+  bucket = data.aws_s3_bucket.artefact_bucket.id
+  key    = "application.zip"
+  depends_on = [null_resource.upload_lambda_zip]
+}
