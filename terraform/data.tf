b10873063b6b728630d7fcbcbbba64c52ca8e2b7
Contoso <91586643+Acontoso@users.noreply.github.com>
2025-08-22 15:16:36 +1000

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,7 +7,12 @@ data "aws_s3_bucket" "artefact_bucket" {
 }
 
 data "aws_s3_object" "lambda_zip" {
-  bucket = data.aws_s3_bucket.artefact_bucket.id
-  key    = "application.zip"
+  #Reference the object in the S3 bucket after the upload has successfully completed
+  bucket     = data.aws_s3_bucket.artefact_bucket.id
+  key        = "application.zip"
   depends_on = [null_resource.upload_lambda_zip]
+}
+
+data "aws_cognito_identity_pool" "identity_pool_oidc" {
+  identity_pool_name = "azure-ad-oidc-sentinel"
 }
